804caf0d5fc68ff600cd61972d35b8edea627b9e
Arshdeep Arora <arsharora017@gmail.com>
2023-06-06 13:50:04 -0400

    add Book constructor

--- library.js
+++ library.js
@@ -1,7 +1,30 @@
 let myLibrary = [];
 
-function Book() {}
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
 
 function addBookToLibrary() {
-  // do stuff here
+  let title = document.querySelector("#title").value;
+  let author = document.querySelector("#author").value;
+  let pages = document.querySelector("#pages").value;
+  let read = document.querySelector("#read").value;
+  let newBook = new Book(title, author, pages, read);
 }
+
+let newBookBtn = document.querySelector("#new-book-btn");
+newBookBtn.addEventListener("click", function () {
+  let newBookForm = document.querySelector("#new-book-form");
+  console.log(newBookForm);
+  newBookForm.style.display = "block";
+});
+
+document
+  .querySelector("#new-book-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    addBookToLibrary();
+  });
