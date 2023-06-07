7abd67d7d6836b1fddddeb4acd06893e343e1729
Arshdeep Arora <arsharora017@gmail.com>
2023-06-06 22:32:38 -0400

    Fix card layout, add function to remove and toggleRead buttons

--- library.js
+++ library.js
@@ -7,18 +7,52 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
+Book.prototype.toggleRead = function () {
+  this.read = !this.read;
+};
+
+function toggleReadBtn(index) {
+  myLibrary[index].toggleRead();
+  render();
+}
+
+function render() {
+  let libraryEl = document.querySelector("#library");
+  libraryEl.innerHTML = "";
+  for (let i = 0; i < myLibrary.length; i++) {
+    let book = myLibrary[i];
+    let bookEl = document.createElement("div");
+    bookEl.setAttribute("class", "book-card");
+    bookEl.innerHTML = `
+    <h2 class="book-title">${book.title}</h2>
+    <h3 class="book-author">by ${book.author}</h3>
+    <p class="book-pages">${book.pages} pages</p>
+    <p class="read-status">${book.read ? "Read" : "Not Read Yet"}</p>
+    <button class="remove-btn" onClick="removeBook(${i})">Remove</button>
+    <button class="toggle-read-btn" onClick="toggleReadBtn(${i})">Toggle Read</button></div>
+    `;
+    libraryEl.appendChild(bookEl);
+  }
+}
+
+function removeBook(index) {
+  myLibrary.splice(index, 1);
+  render();
+}
+
 function addBookToLibrary() {
   let title = document.querySelector("#title").value;
   let author = document.querySelector("#author").value;
   let pages = document.querySelector("#pages").value;
-  let read = document.querySelector("#read").value;
+  let read = document.querySelector("#read").checked;
   let newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  render();
 }
 
 let newBookBtn = document.querySelector("#new-book-btn");
 newBookBtn.addEventListener("click", function () {
   let newBookForm = document.querySelector("#new-book-form");
-  console.log(newBookForm);
   newBookForm.style.display = "block";
 });
 
